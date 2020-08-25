d82ee66cb8c0468d5883349a6bdf785e9f1a2341
Cory Weller <cory.a.weller@gmail.com>
2020-08-25 13:58:25 -0400

    fix map reads

--- Test_Run/map_reads.sh
+++ Test_Run/map_reads.sh
@@ -1,48 +1,37 @@
 #!/usr/bin/env bash
-#SBATCH -N 1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem 64000
-#SBATCH -t 0-24:00:00
-#SBATCH -p economy
-#SBATCH --account berglandlab
+# Run as: bash ./map_reads.sh ${FILE_ID} ${REFGENOME_FILENAME} ${SORTED_VCF}
+# e.g.    bash ./map_reads.sh 4CB-1-82_S82 dmel-all-chromosome-r5.57.fasta dgrp2.sorted.vcf
 
 UTILS_SIF="../utils.sif"
 HARP_SIF="../harp.sif"
-java8="/usr/lib/jvm/java-8-openjdk-amd64/bin/java"
-java11="/usr/lib/jvm/java-11-openjdk-amd64/bin/java"
-
-java8="/opt/java8"
-java11="/opt/java11"
+java8="/usr/lib/jvm/java-8-openjdk-amd64/bin/java" 
+# java11="/usr/lib/jvm/java-11-openjdk-amd64/bin/java" Equivalent to `java`
 
 FILE_ID=${1}
 REFGENOME_FILENAME=${2}
-BGZIP_VCF=${3}
+SORTED_VCF=${3}
 
-FILE_ID="4CB-1-73_S73"
-REFGENOME_FILENAME="dmel-all-chromosome-r5.57.fasta"
-BGZIP_VCF="dgrp2.vcf.gz"
-VCF_FILESTEM=${BGZIP_VCF%.gz}
-VCF_FILESTEM=${VCF_FILESTEM%.vcf}
+VCF_FILESTEM=${SORTED_VCF%.sorted.vcf}
 
 # run PEAR
 singularity exec ${UTILS_SIF} /opt/pear \
-    -f "$file_id"_R1_001.fastq \
-    -r "$file_id"_R2_001.fastq \
-    -o "$file_id"
+    -f "$FILE_ID"_R1_001.fastq \
+    -r "$FILE_ID"_R2_001.fastq \
+    -o "$FILE_ID"
 
 # Map unassembled reads to reference genome
 singularity exec ${UTILS_SIF} bwa mem \
     ${REFGENOME_FILENAME} \
     ${FILE_ID}.unassembled.forward.fastq ${FILE_ID}.unassembled.reverse.fastq | \
     singularity exec ${HARP_SIF} samtools view -Shu - | \
-    singularity exec ${HARP_SIF} samtools sort - "$file_id".unassembled.sorted
+    singularity exec ${HARP_SIF} samtools sort - "$FILE_ID".unassembled.sorted
 
 # Map assembled reads to reference genome
 singularity exec ${UTILS_SIF} bwa mem \
     ${REFGENOME_FILENAME} \
     ${FILE_ID}.assembled.fastq | \
     singularity exec ${HARP_SIF} samtools view -Shu - | \
-    singularity exec ${HARP_SIF} samtools sort - "$file_id".assembled.sorted
+    singularity exec ${HARP_SIF} samtools sort - "$FILE_ID".assembled.sorted
 
 # Merge assembled and unassembled bam files
 singularity exec ${HARP_SIF} samtools merge \
@@ -85,7 +74,7 @@ singularity exec ${UTILS_SIF} ${java8} -Xmx2G -jar /opt/gatk3.jar \
     -I ${FILE_ID}.merged.markdup.rg.bam \
     -o ${FILE_ID}.intervals
 
-# Sort BAM
+# Reorder BAM
 singularity exec ${UTILS_SIF} java -Xmx2G -jar /opt/gatk4.jar ReorderSam \
     -I ${FILE_ID}.merged.markdup.rg.bam \
     -O ${FILE_ID}.merged.markdup.rg.reordered.bam \
@@ -102,3 +91,4 @@ singularity exec ${UTILS_SIF} ${java8} -jar /opt/gatk3.jar \
     -targetIntervals ${FILE_ID}.intervals \
     -o ${FILE_ID}.merged.markdup.rg.realign.bam
 
+# Clean Up
