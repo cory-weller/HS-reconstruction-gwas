bfbc78917f9681a395a06e0cda8c1f8158003f36
Cory Weller <cory.a.weller@gmail.com>
2020-08-26 15:13:10 -0400

    update data prep

--- Test_Run/prep_data.sh
+++ Test_Run/prep_data.sh
@@ -2,9 +2,21 @@
 
 UTILS_SIF="../utils.sif"
 HARP_SIF="../harp.sif"
-VCF_URL="http://dgrp2.gnets.ncsu.edu/data/website/dgrp2.vcf"
+
+
+# VCF_URL="http://dgrp2.gnets.ncsu.edu/data/website/dgrp2.vcf" # gzipped version from zenodo faster to download (equivalent md5 sum)
+VCF_URL="https://zenodo.org/api/files/eef9cec5-1bf4-498e-8ac6-e90aa8c8ab1c/dgrp2.vcf.gz"
+VCF_MD5="af831816f41fe4a779e9abb44c5cdc62"
+VCF_BGZIP_MD5="f4d8551dacf10b6e790d53deeed1f02a"
+VCF_GZIP_MD5="a9ece6b8c4c6b8aaf6797874eaddb369"
 VCF_FILENAME=$(singularity exec ${UTILS_SIF} basename ${VCF_URL})
+VCF_SORTED_MD5=""
+
+# Reference Genome: Drosophila melanogaster release 5, version 57
 REFGENOME_URL="ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/dmel_r5.57_FB2014_03/fasta/dmel-all-chromosome-r5.57.fasta.gz"
+REFGENOME_FILENAME=$(singularity exec ${UTILS_SIF} basename ${REFGENOME_URL})
+REFGENOME_GZIP_MD5="f6e168a1fe8741be2fdce6c2d0602b41"
+REFGENOME_MD5="203ef91d30c76497cd1391a0d2f92827"
 
 # download bam files
 # wget -O 73.test_run.bam "https://onedrive.live.com/download?cid=77DD71E598E5B51B&resid=77DD71E598E5B51B%2118194&authkey=AJNA5CF5UX1bsLI"
@@ -25,14 +37,13 @@ singularity exec ${UTILS_SIF} tar -zxvf priors.tar.gz && rm priors.tar.gz
 
 # Download reference genome
 singularity exec ${UTILS_SIF} wget ${REFGENOME_URL}
-REFGENOME_FILENAME=$(singularity exec ${UTILS_SIF} basename ${REFGENOME_URL})
 if [[ ${REFGENOME_FILENAME: -3} == ".gz" ]]; then
   singularity exec ${UTILS_SIF} gunzip ${REFGENOME_FILENAME}
   REFGENOME_FILENAME=${REFGENOME_FILENAME%.gz}
 fi
 
 # Create reference sequence dictionary
-singularity exec ${UTILS_SIF} java -jar /opt/gatk.jar CreateSequenceDictionary \
+singularity exec ${UTILS_SIF} java -jar /opt/gatk4.jar CreateSequenceDictionary \
   -R "${REFGENOME_FILENAME}" \
   -O "${REFGENOME_FILENAME%.fasta}".dict
 
@@ -43,29 +54,65 @@ singularity exec ${HARP_SIF} samtools faidx ${REFGENOME_FILENAME}
 singularity exec ${UTILS_SIF} bwa index ${REFGENOME_FILENAME}
 
 # Download DGRP freeze 2 VCF
-singularity exec ${UTILS_SIF} wget ${VCF_URL}
-singularity exec ${UTILS_SIF} bgzip ${VCF_FILENAME}
-BGZIP_VCF="${VCF_FILENAME}.gz"
+while true; do
+  if [[ -f ${VCF_FILENAME} ]]; then
+    if [[ $(md5sum ${VCF_FILENAME} | awk '{print $1}') == ${VCF_BGZIP_MD5} ]]; then
+      echo "BGZIP VCF is ready"
+      break
+    elif [[ ! $(md5sum ${VCF_FILENAME} | awk '{print $1}') == ${VCF_GZIP_MD5} ]]; then
+      echo "Removing ${VCF_FILENAME} due to corrupted md5 checksum"
+      rm ${VCF_FILENAME}
+      continue
+    elif [[ $(md5sum ${VCF_FILENAME} | awk '{print $1}') == ${VCF_GZIP_MD5} ]]; then
+      echo "Unzipping non-BGZIP file..."
+      gunzip ${VCF_FILENAME}
+    fi
+  elif [[ -f ${VCF_FILENAME%.gz} ]]; then
+    if [[ $(md5sum ${VCF_FILENAME%.gz} | awk '{print $1}') == ${VCF_MD5} ]]; then
+      echo "Compressing with BGZIP..."
+      singularity exec ${UTILS_SIF} bgzip ${VCF_FILENAME%.gz}
+    else
+      echo "Removing ${VCF_FILENAME%.gz} due to corrupted md5 checksum"
+      rm ${VCF_FILENAME%.gz}
+      continue
+    fi
+    continue
+  else
+    echo "Downloading ${VCF_FILENAME}"
+    singularity exec ${UTILS_SIF} wget ${VCF_URL}
+  fi
+done
 
 # Sort VCF
-singularity exec ${UTILS_SIF} java -Xmx2G -jar /opt/gatk4.jar SortVcf \
-    -I ${BGZIP_VCF} \
-    -O ${BGZIP_VCF%.vcf.gz}.sorted.vcf \
-    --SEQUENCE_DICTIONARY ${REFGENOME_FILENAME%.fasta}.dict \
-    --MAX_RECORDS_IN_RAM 100000
+
+if [[ ! -f ${VCF_FILENAME%.vcf.gz}.sorted.vcf ]]; then
+  echo "Sorting VCF..."
+  # dgrp2.sorted.vcf md5sum: dac0ddb8da32d9e4b68458b7aff28caf
+  singularity exec ${UTILS_SIF} java -Xmx2G -jar /opt/gatk4.jar SortVcf \
+      -I ${VCF_FILENAME} \
+      -O ${VCF_FILENAME%.vcf.gz}.sorted.vcf \
+      --SEQUENCE_DICTIONARY ${REFGENOME_FILENAME%.fasta}.dict \
+      --MAX_RECORDS_IN_RAM 100000
+  echo "BGZIP on sorted VCF..."
+  singularity exec ${UTILS_SIF} bgzip ${VCF_FILENAME%.vcf.gz}.sorted.vcf
+  # dgrp2.sorted.vcf.gz md5sum: e8364935cb63d5fc945a072321d8631c
+fi
 
 # Prepare VCF subset without indels
-singularity exec ${UTILS_SIF} awk -v file=${BGZIP_VCF%.vcf.gz}.sorted.noIndel.vcf -F "\t" '{
+singularity exec ${UTILS_SIF} awk -v file=${VCF_FILENAME%.vcf.gz}.sorted.noIndel.vcf -F "\t" '{
 if($0 ~ /^#/ || $3 ~ /_SNP/)
-    print >> file}' < ${BGZIP_VCF%.vcf.gz}.sorted.vcf
+    print >> file}' < <(zcat ${VCF_FILENAME%.vcf.gz}.sorted.vcf.gz)
+
 
 # Prepare VCF subset with only indels
-singularity exec ${UTILS_SIF} awk -v file=${BGZIP_VCF%.vcf.gz}.sorted.indel.vcf -F "\t" '{
+singularity exec ${UTILS_SIF} awk -v file=${VCF_FILENAME%.vcf.gz}.sorted.indel.vcf -F "\t" '{
 if($0 ~ /^#/ || $3 ~ /_DEL/ || $3 ~ /_INS/)
-    print >> file}' < ${BGZIP_VCF%.vcf.gz}.sorted.vcf
+    print >> file}' < <(zcat ${VCF_FILENAME%.vcf.gz}.sorted.vcf.gz)
 
+exit 0
 ### STOP HERE FOR NOW
-
+# dgrp2.sorted.indel.vcf md5: 0fe9296e7c1681697492c9175a43986f
+# dgrp2.sorted.noIndel.vcf md5: c4793c71cf076964441944845ab8b921
 
 
 
@@ -94,11 +141,11 @@ if($0 ~ /^#/ || $3 ~ /_DEL/ || $3 ~ /_INS/)
 
 
 # bgzip subset VCF
-singularity exec ${UTILS_SIF} bgzip ${BGZIP_VCF%.vcf.gz}.noIndel.vcf
-singularity exec ${UTILS_SIF} bgzip ${BGZIP_VCF%.vcf.gz}.indel.vcf
+singularity exec ${UTILS_SIF} bgzip ${VCF_FILENAME%.vcf.gz}.noIndel.vcf
+singularity exec ${UTILS_SIF} bgzip ${VCF_FILENAME%.vcf.gz}.indel.vcf
 
 # tabix index VCF
-singularity exec ${UTILS_SIF} tabix -p vcf ${BGZIP_VCF}
+singularity exec ${UTILS_SIF} tabix -p vcf ${VCF_FILENAME}
 
 
 # split fasta file into separate files
