119a35bd8d4ed984b1f8b2bfe7c011a3e97ac22e
Cory Weller <cory.a.weller@gmail.com>
2020-09-05 19:56:43 -0400

    move harp function to reconstruct.sh

--- Test_Run/prep_data.sh
+++ Test_Run/prep_data.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-UTILS_SIF="../utils.sif"
-HARP_SIF="../harp.sif"
+if command -v realpath &> /dev/null; then
+    echo "command realpath could not be found. Install coreutils?"
+    exit
+fi
+
+UTILS_SIF=$(realpath ../utils.sif)
+HARP_SIF=$(realpath ../harp.sif)
 
 
 # VCF_URL="http://dgrp2.gnets.ncsu.edu/data/website/dgrp2.vcf" # gzipped version from zenodo faster to download (equivalent md5 sum)
@@ -10,7 +15,6 @@ VCF_MD5="af831816f41fe4a779e9abb44c5cdc62"
 VCF_BGZIP_MD5="f4d8551dacf10b6e790d53deeed1f02a"
 VCF_GZIP_MD5="a9ece6b8c4c6b8aaf6797874eaddb369"
 VCF_FILENAME=$(singularity exec ${UTILS_SIF} basename ${VCF_URL})
-VCF_SORTED_MD5=""
 
 # Reference Genome: Drosophila melanogaster release 5, version 57
 REFGENOME_URL="ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/dmel_r5.57_FB2014_03/fasta/dmel-all-chromosome-r5.57.fasta.gz"
@@ -31,12 +35,12 @@ singularity exec ${UTILS_SIF} wget -O low_coverage_reads.zip "https://onedrive.l
 singularity exec ${UTILS_SIF} unzip low_coverage_reads.zip && \
 rm low_coverage_reads.zip
 
-# TODO change this to conversion from dgrp2 vcf file
-singularity exec ${UTILS_SIF} wget -O priors.tar.gz "https://onedrive.live.com/download?cid=77DD71E598E5B51B&resid=77DD71E598E5B51B%2118196&authkey=AARukAbSz23Cz5w"
-singularity exec ${UTILS_SIF} tar -zxvf priors.tar.gz && rm priors.tar.gz 
+# These link to old harp csv files; they are now generated explicitly from raw data, below
+# singularity exec ${UTILS_SIF} wget -O priors.tar.gz "https://onedrive.live.com/download?cid=77DD71E598E5B51B&resid=77DD71E598E5B51B%2118196&authkey=AARukAbSz23Cz5w"
+# singularity exec ${UTILS_SIF} tar -zxvf priors.tar.gz && rm priors.tar.gz 
 
 # Download reference genome
-singularity exec ${UTILS_SIF} wget ${REFGENOME_URL}
+wget ${REFGENOME_URL}
 if [[ ${REFGENOME_FILENAME: -3} == ".gz" ]]; then
   singularity exec ${UTILS_SIF} gunzip ${REFGENOME_FILENAME}
   REFGENOME_FILENAME=${REFGENOME_FILENAME%.gz}
@@ -109,8 +113,7 @@ singularity exec ${UTILS_SIF} awk -v file=${VCF_FILENAME%.vcf.gz}.sorted.indel.v
 if($0 ~ /^#/ || $3 ~ /_DEL/ || $3 ~ /_INS/)
     print >> file}' < <(zcat ${VCF_FILENAME%.vcf.gz}.sorted.vcf.gz)
 
-exit 0
-### STOP HERE FOR NOW
+
 # dgrp2.sorted.indel.vcf md5: 0fe9296e7c1681697492c9175a43986f
 # dgrp2.sorted.noIndel.vcf md5: c4793c71cf076964441944845ab8b921
 
@@ -148,95 +151,13 @@ singularity exec ${UTILS_SIF} java -Xmx2G -jar /opt/gatk4.jar SelectVariants \
   --output ${VCF_FILENAME%.vcf.gz}.sorted.noIndel.noRep.vcf
 
 
-# Generate HARP genotype csv
+# Generate HARP genotype csv files
 singularity exec ${UTILS_SIF} python3 generate_harp_csv.py ${VCF_FILENAME%.vcf.gz}.sorted.noIndel.noRep.vcf
 
 
-# Generate associative array for chromosome lengths
-awk '{print $1,$2}' dmel-all-chromosome-r5.57.fasta.fai
 
-
-# Read chromosome lengths into associative array from the fasta index
-declare -A LENGTHS
-while read -r CHROM LENGTH; do
-  LENGTHS[${CHROM}]=${LENGTH}
-done < <(awk '{print $1,$2}' ${REFGENOME_FILENAME}.fai)
-
-
-# harp parameters
-HARP_STEP=100000
-HARP_WIDTH=100000
-chromosome="2L"
-chrLength="23011544"
-harp="/scratch/caw5cv/genome-reconstruction-revision/04_RABBIT/harp"
-REFGENOME_FILENAME="${projectDir}/input_data/dgrp2.reference.fasta"
-bamFolder="${projectDir}/02_simulate_reads/${population}/"
-priorsGzFile="${projectDir}/04_RABBIT/${population}/${chromosome}.subset.priors.csv.gz"
-
-BAM_FILENAME
-CHROMOSOME
-REFGENOME_FILENAME
-HARP_CSV_FILENAME
-ORIG_DIR=$(pwd)
-function getHarpFreqs {
-  BAM_FILENAME=${1}
-  CHROMOSOME=${2}
-  CHROMOSOME_LENGTH=${LENGTHS[${CHROMOSOME}]}
-
-  CHROMOSOME=${3}
-  tmpWorkDir=${4}
-  outputDir=${5}
-  chrLength=${6}
-  REFGENOME_FILENAME=${7}
-  priors=${8}
-  window_step=${9}
-  window_width=${10}
-  harp=${11}
-
-  HARP_CSV_FILENAME="${CHROMOSOME}.harp.csv"
-
-  BAM="${ORIG_DIR}/${BAM_FILENAME}"
-  REFGENOME="${ORIG_DIR}/${REFGENOME_FILENAME}"
-  HARP_CSV="${ORIG_DIR}/${HARP_CSV_FILENAME}"
-
-  # make tmp work directory
-  echo creating directory "${tmpWorkDir}/${ind}"
-  mkdir -p "${tmpWorkDir}/${ind}" && cd "${tmpWorkDir}/${ind}"
-
-  # Run harp like
-  echo running harp like
-  singularity exec ${UTILS_SIF} /opt/harp like \
-  --bam ${BAM} \
-  --region ${CHROMOSOME}:1-${CHROMOSOME_LENGTH} \
-  --refseq ${REFGENOME} \
-  --snps ${HARP_CSV} \
-  --stem $ind.$CHROMOSOME
-
-  # Run harp freq
-  echo running harp freq
- singularity exec ${UTILS_SIF} /opt/harp freq \
-  --bam ${BAM} \
-  --region ${CHROMOSOME}:1-${CHROMOSOME_LENGTH} \
-  --refseq ${REFGENOME} \
-  --snps ${HARP_CSV} \
-  --stem $ind.$CHROMOSOME \
-  --window_step ${HARP_STEP} \
-  --window_width ${HARP_WIDTH} \
-  --em_min_freq_cutoff 0.0001
-
-  # Cleanup and move out
-  echo cleaning up
-  cp "${ind}.${CHROMOSOME}.freqs" ${outputDir} && \
-  cd ${tmpWorkDir} && \
-  rm -rf ${tmpWorkDir}/${ind}
-  echo done
-}
-
-export -f getHarpFreqs
-
-
-
-
+exit 0
+### STOP HERE FOR NOW
 
 
 
@@ -280,28 +201,3 @@ else if ($1 ~ /^2L/)
 
 module load gatk
 gatk IndexFeatureFile --feature-file variants_het_2L.vcf
-
-# Create Rsubread Index
-sbatch <<EOF
-#!/usr/bin/env bash
-#SBATCH -N 1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem 30G
-#SBATCH -t 0-0:15:00
-#SBATCH -p largemem
-#SBATCH --account berglandlab
-
-Rscript - <<INNER
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-
-if (!require("Rsubread")) {
-  BiocManager::install("Rsubread")
-  library(Rsubread)
-}
-
-  buildindex(basename="dgrp2", reference="${REFGENOME_FILENAME}")
-
-INNER
-EOF
